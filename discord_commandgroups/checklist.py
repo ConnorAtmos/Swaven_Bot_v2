31f6ca641a6f48c21b99397fb092f1cd7467326f
ConnorAtmos <connor.sw.personal@gmail.com>
2023-12-13 21:35:10 -0600

    Checklist

--- discord_commandgroups/checklist.py
+++ discord_commandgroups/checklist.py
@@ -19,11 +19,15 @@ def get_response_str():
     for i in range(len(data)):
         list_item = data[i]
         if not list_item["completed"]:
-            response_str += str(i) + " | " + list_item["name"] + ": " + list_item["description"] + "\n"
+
+            response_str += str(i+1) + ". \t " + list_item["name"]
+            if list_item["description"] != "":
+                response_str += ": " + list_item["description"]
+            response_str += "\n"
     return response_str
 
-@command_group.command(name="get_list", description="Gets the list of items to do")
-async def get_list(ctx):
+@command_group.command(name="list_tasks", description="Gets the list of tasks to do")
+async def list_tasks(ctx):
     await ctx.respond(get_response_str())
 
 @command_group.command(name="create_task", description="Creates a task")
@@ -35,9 +39,10 @@ async def create_task(ctx, name: str, description: str=""):
     await ctx.respond("Added task " + name + " with description " + description)
 
 @command_group.command(name="complete_task", description="Marks a task as complete")
-async def complete_task(ctx, index: int):
+async def complete_task(ctx, number: int):
     global data
 
+    index = number - 1
     found = False
     for i in range(len(data)):
         if i == index:
@@ -48,7 +53,26 @@ async def complete_task(ctx, index: int):
     if found:
         data = database.save("checklist", data)
 
-        await ctx.respond(data[index]["name"] + " marked as complete.")
+        await ctx.respond(str(number) + " | " + data[index]["name"] + " marked as complete.")
+    else:
+        await ctx.respond("Cannot find task from index " + str(index))
+
+@command_group.command(name="un_complete_task", description="Marks a task as incomplete")
+async def un_complete_task(ctx, number: int):
+    global data
+
+    index = number - 1
+    found = False
+    for i in range(len(data)):
+        if i == index:
+            data[i]["complete"] = False
+            found = True
+            break
+
+    if found:
+        data = database.save("checklist", data)
+
+        await ctx.respond(str(number) + " | " + data[index]["name"] + " marked as incomplete.")
     else:
         await ctx.respond("Cannot find task from index " + str(index))
 
